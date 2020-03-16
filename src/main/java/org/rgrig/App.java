63ead7aea42c27e0f70ae9ed3d6b409e5e0cc6c2
David W. Arnold <d.w.arnold.gm@gmail.com>
2020-03-16 02:51:32 +0000

    Ready to submit code

--- src/main/java/org/rgrig/App.java
+++ src/main/java/org/rgrig/App.java
@@ -10,9 +10,9 @@ public class App
     public static void main(final String[] args) throws URISyntaxException
     {
         // Submission Server
-        WebSocketClient client = new Client(new URI("ws://129.12.44.246:1234"), "dwa4", "597feceb");
+        // WebSocketClient client = new Client(new URI("ws://129.12.44.246:1234"), "dwa4", "597feceb");
         // Test Server (View Scores: http://129.12.44.229/)
-        // WebSocketClient client = new Client(new URI("ws://129.12.44.229:1234"), "dwa4", "597feceb");
+        WebSocketClient client = new Client(new URI("ws://129.12.44.229:1234"), "dwa4", "597feceb");
         client.connect();
     }
 }

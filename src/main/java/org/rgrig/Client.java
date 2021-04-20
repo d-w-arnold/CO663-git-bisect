708f9df99b407551341e8a9028c03f4a90ede75c
David W. Arnold <d.w.arnold.gm@gmail.com>
2021-04-20 13:18:37 +0100

    Simplified code and added better commenting

--- src/main/java/org/rgrig/Client.java
+++ src/main/java/org/rgrig/Client.java
@@ -10,45 +10,43 @@ import java.util.*;
 
 class Client extends WebSocketClient
 {
-    State state = State.START;
-    String goodCommit;
-    String badCommit;
-    List<String> queue;
-    Map<String, Integer> rankings;
-    Map<String, Set<String>> ancestors;
-    String latestAskedCommit;
-    String mostLikelySolution;
-    String commitToAsk;
-    boolean foundCommitToAsk;
-    List<String> breadthFirst;
-    Set<String> visitedInBreathFirst;
-    Map<String, Set<String>> parents;
-    Map<String, Boolean> answeredCommits;
-    int interval;
-    int threshold;
-    int count;
-    int parentsSize;
-    int batch;
-    String kentId;
-    String token;
-    String repoName;
-    int instanceCount;
-    JSONArray jsonDag;
-    Map<String, Set<String>> parentsOriginal;
-    int instanceCounter;
-    int totalQuestionCounter;
+    private final String kentId;
+    private final String token;
+    private State state = State.START;
+    private String goodCommit;
+    private String badCommit;
+    private List<String> queue;
+    private Map<String, Integer> rankings;
+    private Map<String, Set<String>> ancestors;
+    private String latestAskedCommit;
+    private String mostLikelySolution;
+    private String commitToAsk;
+    private boolean foundCommitToAsk;
+    private List<String> breadthFirst;
+    private Set<String> visitedInBreathFirst;
+    private Map<String, Set<String>> parents;
+    private Map<String, Boolean> answeredCommits;
+    private int interval;
+    private int threshold;
+    private int count;
+    private int parentsSize;
+    private int batch;
+    private String repoName;
+    private JSONArray jsonDag;
+    private Map<String, Set<String>> parentsOriginal;
+    private int instanceCounter;
+    private int totalQuestionCounter;
 
-    Client(final URI server, final String kentId, final String token)
+    public Client(final URI server, final String kentId, final String token)
     {
         super(server);
         this.kentId = kentId;
         this.token = token;
-        repoName = null;
-        instanceCount = 0;
-        jsonDag = null;
-        parentsOriginal = null;
-        instanceCounter = 0;
-        totalQuestionCounter = 0;
+        this.repoName = null;
+        this.jsonDag = null;
+        this.parentsOriginal = null;
+        this.instanceCounter = 0;
+        this.totalQuestionCounter = 0;
     }
 
     @Override
@@ -57,67 +55,111 @@ class Client extends WebSocketClient
         final JSONObject message = new JSONObject(messageText);
         switch (state) {
             case START:
-                if (message.has("Repo")) {
-                    JSONObject jsonRepo = message.getJSONObject("Repo");
-                    repoName = jsonRepo.getString("name");
-                    instanceCount = jsonRepo.getInt("instance_count");
-                    jsonDag = jsonRepo.getJSONArray("dag");
-                    genCommitsAndParentsMap(jsonDag);
-                    instanceCounter = 0;
-                } else if (message.has("Instance")) {
-                    if (repoName == null) {
-                        System.err.println("Protocol error: instance without having seen a repo.");
-                        close();
-                    }
-                    instanceCounter++;
-                    parents = parentsOriginal;
-                    JSONObject jsonInstance = message.getJSONObject("Instance");
-                    goodCommit = jsonInstance.getString("good");
-                    badCommit = jsonInstance.getString("bad");
-                    System.out.printf("Solving instance (good %s; bad %s) of %s (%d) (Total Questions Asked: %d)\n", goodCommit, badCommit, repoName, instanceCounter, totalQuestionCounter);
-                    latestAskedCommit = null;
-                    answeredCommits = new HashMap<>()
-                    {{
-                        put(goodCommit, true);
-                        put(badCommit, false);
-                    }};
-                    mostLikelySolution = badCommit;
-                    commitToAsk = null;
-                    foundCommitToAsk = false;
-                    trimTheFat();
-                    parentsSize = parents.size();
-                    threshold = 10000;
-                    batch = 100;
-                    interval = parentsSize / batch;
-                    genBreadthAndRanking();
-                    state = State.IN_PROGRESS;
-                    questionOrSolution();
-                } else if (message.has("Score")) {
-                    close();
-                } else {
-                    System.err.println("Unexpected message while waiting for a problem.");
-                    close();
-                }
+                startState(message);
                 break;
             case IN_PROGRESS:
-                if (message.has("Answer")) {
-                    if (message.get("Answer").equals("Good")) {
-                        answerGood();
-                    } else if (message.get("Answer").equals("Bad")) {
-                        answerBad();
-                    }
-                    questionOrSolution();
-                } else {
-                    System.err.println("Unexpected message while in-progress.");
-                    close();
-                }
+                inProgressState(message);
                 break;
             default:
                 assert false;
         }
     }
 
-    // Generate the commits array in order to generate the parents hashmap (key = commit, value = list of parents)
+    /**
+     * Start state process.
+     *
+     * @param message The message received as a JSON object.
+     */
+    private void startState(final JSONObject message)
+    {
+        if (message.has("Repo")) {
+            startStateRepo(message);
+        } else if (message.has("Instance")) {
+            startStateInstance(message);
+        } else if (message.has("Score")) {
+            close();
+        } else {
+            System.err.println("Unexpected message while waiting for a problem.");
+            close();
+        }
+    }
+
+    /**
+     * Start state process, when the message contains a repo.
+     *
+     * @param message The message received as a JSON object.
+     */
+    private void startStateRepo(final JSONObject message)
+    {
+        JSONObject jsonRepo = message.getJSONObject("Repo");
+        repoName = jsonRepo.getString("name");
+        jsonDag = jsonRepo.getJSONArray("dag");
+        genCommitsAndParentsMap(jsonDag);
+        instanceCounter = 0;
+    }
+
+    /**
+     * Start state process, when the message contains an instance.
+     *
+     * @param message The message received as a JSON object.
+     */
+    private void startStateInstance(final JSONObject message)
+    {
+        if (repoName == null) {
+            System.err.println("Protocol error: instance without having seen a repo.");
+            close();
+        }
+        instanceCounter++;
+        parents = parentsOriginal;
+        JSONObject jsonInstance = message.getJSONObject("Instance");
+        goodCommit = jsonInstance.getString("good");
+        badCommit = jsonInstance.getString("bad");
+        System.out.printf("Solving instance (good %s; bad %s) of %s (%d) (Total Questions Asked: %d)\n", goodCommit, badCommit, repoName, instanceCounter, totalQuestionCounter);
+        latestAskedCommit = null;
+        answeredCommits = new HashMap<>()
+        {{
+            put(goodCommit, true);
+            put(badCommit, false);
+        }};
+        mostLikelySolution = badCommit;
+        commitToAsk = null;
+        foundCommitToAsk = false;
+        trimTheFat();
+        parentsSize = parents.size();
+        threshold = 10000;
+        batch = 100;
+        interval = parentsSize / batch;
+        genBreadthAndRanking();
+        state = State.IN_PROGRESS;
+        questionOrSolution();
+    }
+
+
+    /**
+     * In-progress state process.
+     *
+     * @param message The message received as a JSON object.
+     */
+    private void inProgressState(final JSONObject message)
+    {
+        if (message.has("Answer")) {
+            if (message.get("Answer").equals("Good")) {
+                answerGood();
+            } else if (message.get("Answer").equals("Bad")) {
+                answerBad();
+            }
+            questionOrSolution();
+        } else {
+            System.err.println("Unexpected message while in-progress.");
+            close();
+        }
+    }
+
+    /**
+     * Generate the commits array in order to generate the parents hashmap (key = commit, value = list of parents).
+     *
+     * @param jsonDag The JSON array containing relationships between commits.
+     */
     private void genCommitsAndParentsMap(JSONArray jsonDag)
     {
         parentsOriginal = new HashMap<>();
@@ -132,7 +174,9 @@ class Client extends WebSocketClient
         }
     }
 
-    // Get rid of commits which aren't in the ancestry of the badCommit
+    /**
+     * Get rid of commits which aren't in the ancestry of the badCommit.
+     */
     private void trimTheFat()
     {
         genBreadthFirstStack(badCommit);
@@ -144,7 +188,9 @@ class Client extends WebSocketClient
         parents = newParents;
     }
 
-    // Generate list of commits from breadth first search, either all or some commits at certain intervals
+    /**
+     * Generate list of commits from breadth first search, either all or some commits at certain intervals.
+     */
     private void genBreadthAndRanking()
     {
         if (parentsSize > threshold) {
@@ -171,7 +217,11 @@ class Client extends WebSocketClient
         }
     }
 
-    // Generate breath first (search) stack
+    /**
+     * Generate breath first (search) stack.
+     *
+     * @param commit The commit to start the search.
+     */
     private void genBreadthFirstStackAtInterval(String commit)
     {
         breadthFirst = new ArrayList<>();
@@ -202,7 +252,9 @@ class Client extends WebSocketClient
         }
     }
 
-    // Generate breath first (search) stack
+    /**
+     * Generate breath first (search) stack.
+     */
     private void genBreadthFirstStack(String commit)
     {
         breadthFirst = new ArrayList<>();
@@ -250,7 +302,9 @@ class Client extends WebSocketClient
         }
     }
 
-    // Generate scores for all commits in breadth first stack, which aren't already known to be good/bad
+    /**
+     * Generate scores for all commits in breadth first stack, which aren't already known to be good/bad.
+     */
     private void genRankings()
     {
         rankings = new HashMap<>();
@@ -264,7 +318,9 @@ class Client extends WebSocketClient
         }
     }
 
-    // Decide whether to ask a question or send a solution
+    /**
+     * Decide whether to ask a question or send a solution.
+     */
     private void questionOrSolution()
     {
         if (rankings.isEmpty()) {
@@ -280,7 +336,13 @@ class Client extends WebSocketClient
         }
     }
 
-    // We haven't found optimal commit to ask, so find commit with best score/ranking to question
+    //
+
+    /**
+     * We haven't found optimal commit to ask, so find commit with best score/ranking to question.
+     *
+     * @return The commit to query/question next.
+     */
     private String commitToQuestion()
     {
         int halfway = breadthFirst.size() / 2;
@@ -304,7 +366,9 @@ class Client extends WebSocketClient
         send(new JSONObject().put("Question", commit).toString());
     }
 
-    // Good answer received
+    /**
+     * Good answer received.
+     */
     private void answerGood()
     {
         answeredCommits.put(latestAskedCommit, true);
@@ -329,7 +393,9 @@ class Client extends WebSocketClient
         genBreadthAndRanking();
     }
 
-    // Bad answer received
+    /**
+     * Bad answer received.
+     */
     private void answerBad()
     {
         answeredCommits.put(latestAskedCommit, false);
